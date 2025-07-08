f2f2cf223ad2fcee2a13a158d613a506232c0b03
jona-sassenhagen <jona.sassenhagen@neocybernetica.com>
2025-07-08 10:56:45 +0200

    add and comment out stump for spell it out game

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,18 +1,17 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
 
 import translationEN from './locales/en/translation.json'
 import translationDE from './locales/de/translation.json'
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: translationEN },
       de: { translation: translationDE },
     },
+    lng: 'de', // Force language to German
     fallbackLng: 'en',
     interpolation: { escapeValue: false },
   })
